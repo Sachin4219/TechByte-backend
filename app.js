8138a9fec05e425537748bd5022d07cb7cc58b8c
Sachin4219 <elzachin373@gmail.com>
2023-05-25 20:00:13 +0530

    Finishing up

--- app.js
+++ app.js
@@ -2,43 +2,41 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./api/index.js";
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 // Configurations
-app.use( express.json() );
-app.use( cors() );
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
 // Log Requests
-app.use( ( req, res, next ) => {
+app.use((req, res, next) => {
   // green | yellow | white
   console.log(
     "\x1b[32m",
-    `\b[${ req.method }]`,
+    `\b[${req.method}]`,
     "\x1b[33m",
-    `\b${ req.path }`,
+    `\b${req.path}`,
     "\x1b[37m"
   );
   next();
-} );
+});
 
 const MONGO_URI = process.env.MONGO_URI;
 // Database connection
 mongoose
-  .connect( MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true } )
-  .then( () =>
-    app.listen( PORT, () => {
-      console.log( `Server Running on Port ${ PORT }` );
-    } )
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server Running on Port ${PORT}`);
+    })
   )
-  .catch( ( error ) => {
-    console.log( `${ error } did not connect` );
-  } );
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
 
-app.use( "/", router );
+app.use("/", router);
--- controllers/authors.js
+++ controllers/authors.js
@@ -3,96 +3,102 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { Response } from "../types/response.js";
 
-export const generateToken = ( author ) => {
-  console.log( author );
-  return jwt.sign( { authorname: author.username, id: author._id }, "kadabra", {
+export const generateToken = (author) => {
+  console.log(author);
+  return jwt.sign({ authorname: author.username, id: author._id }, "kadabra", {
     expiresIn: "360min",
-  } );
+  });
 };
 
-export const register = async ( req, res ) => {
-  const { name, username, password, email } = req.body;
-  const serviceResponse = { ...Response }
+export const register = async (req, res) => {
+  const { name, username, bio, photo, password, email } = req.body;
+  const serviceResponse = { ...Response };
   try {
     // Find if author already exists or not
-    const foundAuthor = await Author.findOne( { username } )
+    const foundAuthor = await Author.findOne({ username });
     // If author already exists
-    if ( foundAuthor ) {
-      serviceResponse.msg = "author is already registered"
-      res.status( 409 )
-        .json( serviceResponse );
+    if (foundAuthor) {
+      serviceResponse.msg = "author is already registered";
+      res.status(409).json(serviceResponse);
     } else {
       // Create hash
-      const hash = await bcrypt.hash( password, 10 );
+      const hash = await bcrypt.hash(password, 10);
       // console.log("Hash created", hash)
 
-      const newAuthor = new Author( { name, username, password: hash, email } );
+      const newAuthor = new Author({
+        name,
+        username,
+        bio,
+        photo,
+        password: hash,
+        email,
+      });
       await newAuthor.save();
 
-      serviceResponse.success = true
-      serviceResponse.msg = "Author registered successfully"
+      serviceResponse.success = true;
+      serviceResponse.msg = "Author registered successfully";
       serviceResponse.response = {
         name: newAuthor.username,
+        photo: newAuthor.photo,
         email: newAuthor.email,
-        token: generateToken( newAuthor ),
-      }
+        token: generateToken(newAuthor),
+      };
 
-      res.status( 201 ).json( serviceResponse );
+      res.status(201).json(serviceResponse);
     }
-  }
-  catch ( error ) {
+  } catch (error) {
     serviceResponse.msg = "Failed to register author";
     serviceResponse.error = error;
-    res.status( 500 ).json( serviceResponse );
+    res.status(500).json(serviceResponse);
   }
 };
 
-export const login = async ( req, res ) => {
+export const login = async (req, res) => {
   const { email, password } = req.body;
   // console.log(req.body)
-  const serviceResponse = { ...Response }
+  const serviceResponse = { ...Response };
   try {
-    const foundAuthor = await Author.findOne( { email } );
+    const foundAuthor = await Author.findOne({ email });
     const hash = foundAuthor.password;
-    bcrypt.compare( password, hash, ( err, result ) => {
-      if ( err ) console.log( err );
+    bcrypt.compare(password, hash, (err, result) => {
+      if (err) console.log(err);
       else {
-        if ( result ) {
+        if (result) {
           serviceResponse.response = {
             name: foundAuthor.username,
+            photo: foundAuthor.photo,
             email: foundAuthor.email,
-            token: generateToken( foundAuthor ),
-          }
-          serviceResponse.success = true
-          serviceResponse.msg = "Author logged in successfully"
+            token: generateToken(foundAuthor),
+          };
+          serviceResponse.success = true;
+          serviceResponse.msg = "Author logged in successfully";
 
-          res.status( 200 ).json( serviceResponse );
+          res.status(200).json(serviceResponse);
         } else {
           // console.log("Password Does not match");
-          serviceResponse.msg = "Password does not match (Invalid credentials)"
-          res.status( 401 ).json( serviceResponse );
+          serviceResponse.msg = "Password does not match (Invalid credentials)";
+          res.status(401).json(serviceResponse);
         }
       }
-    } );
-  } catch ( error ) {
+    });
+  } catch (error) {
     serviceResponse.msg = "Failed to login author";
     serviceResponse.error = error;
-    res.status( 500 ).json( serviceResponse );
+    res.status(500).json(serviceResponse);
   }
 };
 
-export const check_auth = ( req, res, next ) => {
-  console.log( "Cheking Auth" );
-  const serviceResponse = { ...Response }
+export const check_auth = (req, res, next) => {
+  console.log("Cheking Auth");
+  const serviceResponse = { ...Response };
   try {
-    const token = req.headers.authorization.split( " " )[ 1 ];
-    var decoded = jwt.verify( token, "kadabra" );
+    const token = req.headers.authorization.split(" ")[1];
+    var decoded = jwt.verify(token, "kadabra");
     req.authorData = decoded;
     next();
-  } catch ( error ) {
-    serviceResponse.msg = "Unauthorized Access"
+  } catch (error) {
+    serviceResponse.msg = "Unauthorized Access";
     serviceResponse.response = { isVerified: false };
-    return res.status( 401 ).json( serviceResponse );
+    return res.status(401).json(serviceResponse);
   }
 };
-
